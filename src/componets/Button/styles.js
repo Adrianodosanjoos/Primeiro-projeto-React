0df4aae8cae951e12717f5111e7b047d6a19c561
adrianodosanjoos <adriano.luizysa@gmail.com>
2024-08-07 18:27:58 -0300

    project atualizado

--- src/componets/Button/styles.js
+++ src/componets/Button/styles.js
@@ -1,48 +1,37 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-width: 342px;
-height: 74px;
-margin-top: 130px;
+  width: 342px;
+  height: 74px;
+  margin-top: 130px;
 
-background: ${props => props.isback ? 'trasnparent' : ' rgba(0, 0, 0.8)'};
-border-radius: 14px;
-border: ${props => props.isback ? '1px solid #ffffff' : 'none'} ;
+  background: ${(props) => (props.isback ? 'transparent' : 'rgba(0, 0, 0, 0.8)')};
+  border-radius: 14px;
+  border: ${(props) => (props.isback ? '1px solid #ffffff' : 'none')};
 
-font-style: normal;
-font-weight: bold;
-font-size: 17px;
-line-height: 28px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 17px;
+  line-height: 28px;
+  color: #ffffff;
 
-color: #ffffffff;
+  cursor: pointer;
 
-cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
 
-display: flex;
-align-items: center;
-justify-content: center;
-gap: 20px;
+  &:hover {
+    opacity: 0.8;
+  }
 
-&:hover{
-  opacity: 0.8;
-}
+  &:active {
+    opacity: 0.5;
+  }
 
-&:active{
-  opacity: 0.5;
-}
-
-
-img{
-    transform: ${props => props.isback && 'rotateY(180deg)' };
-    
-}
-
+  img {
+    transform: ${(props) => props.isback && 'rotateY(180deg)'};
+  }
 `;
-
-
-
-
-
-
-
 
